a413ff4b6747e8e62a3f97e23b4b1610fff3f09d
Josiah Kerley <josiahkerley@gmail.com>
2016-06-18 21:55:15 -0600

    triggering ci

--- tests/10-generate_files.py
+++ tests/10-generate_files.py
@@ -21,6 +21,32 @@ def test_generateTemplate():
   template = FileGen.Template(filepath='test.jn2')
   params   = FileGen.Parameters(filepaths=['test.yml'])
   result   = template.render(params=params.params)
+  os.remove('test.yml')
+  os.remove('test.jn2')
+  return(result)
+
+
+def test_generateTemplateConfirmOutput():
+  parameters = {
+    'list':[
+      1,
+      2,
+      3
+    ]
+  }
+  template_text = '''This is a test.
+{% for l in list %}{{ l }}
+{% endfor %}'''
+  import FileGen
+  import yaml
+  import os
+  with open('test.jn2','w') as f:
+    f.write(template_text)
+  with open('test.yml','w') as f:
+    f.write(yaml.dump(parameters))
+  template = FileGen.Template(filepath='test.jn2')
+  params   = FileGen.Parameters(filepaths=['test.yml'])
+  result   = template.render(params=params.params)
   print result
   os.remove('test.yml')
   os.remove('test.jn2')
