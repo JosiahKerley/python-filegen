a221436371642ae364ef69edea3795d67240ce72
Josiah Kerley <josiahkerley@gmail.com>
2016-06-18 19:53:28 -0600

    Init

---
+++ FileGen/__init__.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python
+import os
+import ast
+import json
+import yaml
+import jinja2
+import argparse
+import commands
+
+
+## Exceptions
+class FileNotFound(Exception):
+  pass
+class CommandFailed(Exception):
+  pass
+
+
+## Params
+class Parameters:
+
+  filepaths = None
+  debug = None
+  ignore_cmd_err = None
+
+
+  def __init__(self,filepaths,ignore_cmd_err=False,debug=False):
+    self.filepaths = filepaths
+    self.debug = debug
+    self.ignore_cmd_err = ignore_cmd_err
+
+
+  def load(self,filepaths):
+    params = {}
+    for i in filepaths:
+      try:
+        assert os.path.isfile(i)
+      except Exception as e:
+        raise FileNotFound('Cannot find file {}'.format(i))
+        print(e)
+      with open(i,'r') as f:
+        param_text = f.read()
+      try:
+        p = yaml.load(param_text)
+        params = dict(params, **p)
+      except:
+        p = json.loads(param_text)
+        params = dict(params, **p)
+
+
+  ## Resolve metaparams
+  def update_tree(self,data,function,namespace=None):
+    if namespace == None:
+      namespace = data
+    if type(data) == type({}):
+      for n in data:
+        data[n] = self.update_tree(data[n],function,namespace)
+    elif type(data) == type([]):
+      c = 0
+      for i in data:
+        data[c] = self.update_tree(i,function,namespace)
+        c += 1
+    else:
+      data = function(data,namespace)
+    return(data)
+
+
+  def resolve_tree(self,data,namespace):
+    data = str(data)
+    data = jinja2.Environment().from_string(data).render(namespace)
+    try:
+      data = ast.literal_eval(data)
+    except:
+      pass
+    if type(data) == type(u''):
+      if data.startswith('<[') and data.endswith(']>'):
+        cmd = data.lstrip('<[').rstrip(']>')
+        status = commands.getstatusoutput(cmd)
+        if not self.ignore_cmd_err:
+          try:
+            assert status[0] == 0
+          except:
+            raise CommandFailed('Command "{}" exited with a nonzero value!'.format(cmd))
+        data = status[-1]
+    return(data)
+
+
+  def resolveYAMLJinja(self,data,evals=25):
+    for i in range(0,evals):
+      data = self.update_tree(data,self.resolve_tree)
+    return(data)
+
+
+
+## Template
+class Template:
+  template_text  = None
+  ignore_cmd_err = None
+  debug = None
+
+
+  def __init__(self,filepath,ignore_cmd_err=False,debug=False):
+    self.ignore_cmd_err = ignore_cmd_err
+    self.debug = debug
+    self.load(filepath)
+
+
+  def load(self,filepath):
+    try:
+      assert os.path.isfile(filepath)
+    except Exception as e:
+      raise FileNotFound('Cannot find file {}'.format(filepath))
+      print(e)
+    with open(filepath,'r') as f:
+      self.template_text = f.read()
+
+
+  def render_jinja(data,namespace):
+    data = str(data)
+    data = jinja2.Environment().from_string(data).render(namespace)
+    try:
+      data = ast.literal_eval(data)
+    except:
+      pass
+    if type(data) == type(u''):
+      if data.startswith('<[') and data.endswith(']>'):
+        cmd = data.lstrip('<[').rstrip(']>')
+        status = commands.getstatusoutput(cmd)
+        if not self.ignore_cmd_err:
+          try:
+            assert status[0] == 0
+          except:
+            raise CommandFailed('Command "{}" exited with a nonzero value!'.format(cmd))
+        data = status[-1]
+    return(data)
+
+
+  def resolveYAMLJinja(data,evals=25):
+    for i in range(0,evals):
+      data = self.update_tree(data,self.render_jinja)
+    return(data)
+
+
+  def render(self,params):
+    ## Render template
+    params = resolveYAMLJinja(params)
+    render = jinja2.Environment().from_string(template_text).render(params)
+    if results.debug:
+      print('{}'.format(json.dumps(params,indent=2)))
+    else:
+      print(render)
+
+
+
+
+## Shell
+class Shell:
+  results = None
+  def __init__(self):
+    parser = argparse.ArgumentParser(description='Generic file template generator')
+    parser.add_argument('--template', '-t',             action="store",      dest="template_path",  default=False, help='Path to template file',                required = True)
+    parser.add_argument('--parameters', '-p',           action="store",      dest="param_paths",    default=False, help='Path to parameter file(s)', nargs='*', required = True)
+    parser.add_argument('--debug', '-d',                action="store_true", dest="debug",          default=False, help='Show the params')
+    parser.add_argument('--ignore-command-error', '-i', action="store_true", dest="ignore_cmd_err", default=False, help='Allows the tool to ignore nonzero exit codes on <[metacommands]>')
+    parser.add_argument('--version', action='version',  version='%(prog)s 1.0')
+    self.results = parser.parse_args()
+    template = Template(filepath=results.template_path,ignore_cmd_err=results.ignore_cmd_err)
+    params   = Parameters(filepaths=results.param_paths,ignore_cmd_err=results.ignore_cmd_err)
+
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import setup
+from pip.req import parse_requirements
+install_reqs = parse_requirements('requirements.txt', session=False)
+requirements = [str(ir.req) for ir in install_reqs]
+setup(
+  name             = 'FileGen',
+  version          = '1.0.0',
+  description      = 'Universal file generation toolbox',
+  author           = 'Josiah Kerley',
+  author_email     = 'josiah@kerley.io',
+  url              = 'http://josiah.kerley.io',
+  install_requires = requirements,
+  packages         = [
+    'FileGen'
+  ],
+  entry_points     = {
+    "console_scripts": [
+      "filegen = FileGen:Shell"
+    ]
+  },
+)
