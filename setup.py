68f5ebefba4aa152751ff3f24d1924b5aa61e298
Josiah Kerley <josiahkerley@gmail.com>
2016-06-18 20:42:03 -0600

    triggering ci

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_reqs = parse_requirements('requirements.txt', session=False)
 requirements = [str(ir.req) for ir in install_reqs]
 setup(
   name             = 'FileGen',
-  version          = '1.0.0',
+  version          = '1.0.1',
   description      = 'Universal file generation toolbox',
   author           = 'Josiah Kerley',
   author_email     = 'josiah@kerley.io',
